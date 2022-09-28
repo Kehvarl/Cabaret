972e1a1270ce553b1ad7d904505e2cf8855f2a83
Kehvarl <Kehvarl@Kehvarl.com>
2022-09-27 17:03:20 -0700

    preparing to play with users

--- user.php
+++ user.php
@@ -5,11 +5,50 @@ class user implements JsonSerializable
     public $id;
     public $name;
     public $email;
-    public $password;
+    public $password_hash;
 
-    public static function check_password($username, $password)
+    /**
+     * @throws Exception
+     */
+    public function __construct($username, $email, $password_hash)
+    {
+        $this->name = $username;
+        $this->email = $email;
+        $this->password_hash = $password_hash;
+        $this->id = random_int(1000,10000);
+    }
+
+    public static function user_exists($username): bool
+    {
+        return false;
+    }
+
+    public static function email_exists($email): bool
+    {
+        return false;
+    }
+
+    public static function check_password($username, $password): bool
     {
         return password_verify($password, password_hash("password", PASSWORD_BCRYPT));
+    }
+
+    /**
+     * @throws Exception
+     */
+    public static function create($username, $email, $password): user
+    {
+        $password_hash = password_hash($password, PASSWORD_BCRYPT);
+        if (user::user_exists($username))
+        {
+            throw new Exception("User Exists");
+        }
+        elseif (user::email_exists($email))
+        {
+            throw new Exception("Email Exists");
+        }
+
+        return new user($username, $email, $password_hash);
     }
 
     /**
