ffc120c0dd494743f5a3d760e16d777165219c55
Kehvarl <Kehvarl@Kehvarl.com>
2022-10-01 18:16:37 -0700

    preparing to play with users

--- test.php
+++ test.php
@@ -15,17 +15,29 @@
 */
 
     require_once ('user.php');
+    require_once ("room.php");
+    require_once ("login.php");
+    $user = null;
     try
     {
         $user = User::create('Kehvarl', 'kehvarl@kehvarl.com', 'PAssword');
-        print_r($user->jsonSerialize());
+        //print_r($user->jsonSerialize());
         //print_r($user->password_hash);
     }
     catch (Exception $e)
     {
         print_r($e);
     }
+    $r = new Room("Main", "Room");
+    $l = new Login($user, "Kehv", "A Dragon", $r);
 
+    $r->addpost(new Post($l->name, "Test", "#c0c0c0"));
+
+    echo "<!doctype html><html class='no-js' lang=''><head><title>Cabaret</title>";
+    echo "<link rel='stylesheet' href='style/style.css' type='text/css'/>";
+    echo "</head><body>\n";
+    echo $r->render();
+    echo "</body></html>";
 
 
 /*
