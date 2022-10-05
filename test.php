5a090e0909d82f5770918f7b8a7661879255e17c
Kehvarl <Kehvarl@Kehvarl.com>
2022-10-04 21:24:07 -0700

    preparing to play with users

--- test.php
+++ test.php
@@ -23,7 +23,7 @@
         $user = User::create('Kehvarl', 'kehvarl@kehvarl.com', 'PAssword');
         //print_r($user->jsonSerialize());
         //print_r($user->password_hash);
-        echo(User::check_password('Kehvarl', 'PAssword'));
+        print(User::check_password('Kehvarl', 'PAssword'));
     }
     catch (Exception $e)
     {
