c060516909862361022db44decafc8e4e540c4ce
Kehvarl <Kehvarl@Kehvarl.com>
2022-10-26 20:11:41 -0700

    Prepping to get users from DB,
    Should I maybe be using Laravel?

--- test.php
+++ test.php
@@ -1,36 +1,45 @@
 
 <?php
-    require_once ('user.php');
-    require_once ("room.php");
-    require_once ("login.php");
-    $user = null;
+require_once ('user.php');
+require_once ("room.php");
+require_once ("login.php");
+
+session_start();
+
+
+if (!isset($_SESSION['user']))
+{
     try
     {
-        $user = User::login('keHVaRL', 'TestPAss');
+        $_SESSION['user'] = User::login('keHVaRL', 'TestPAss');
     }
     catch (Exception $e)
     {
         print_r($e);
+        die('Login Failed');
     }
+}
 
-    try
-    {
-        $r = Room::list($user)[0];
-    }
-    catch (Exception $e)
-    {
-        die("No Room");
-    }
+$user = $_SESSION['user'];
 
-    $l = new Login($user, "Kehv", "A Dragon", $r);
-    $l2 = new Login($user, "Illyrin", "Another Dragon", $r);
+try
+{
+    $r = Room::list($user)[0];
+}
+catch (Exception $e)
+{
+    die("No Room");
+}
 
-    $r->addpost(new Post($user, $l->name, "Testing 123", "#c0c0c0"));
-    $r->addpost(new Post($user, $l2->name, "You know the drill.", "#00c0c0"));
+$l = new Login($user, "Kehv", "A Dragon", $r);
+$l2 = new Login($user, "Illyrin", "Another Dragon", $r);
 
-    echo "<!doctype html><html class='no-js' lang=''><head><title>Cabaret</title>";
-    echo "<link rel='stylesheet' href='style/style.css' type='text/css'/>";
-    echo "</head><body>\n";
-    echo $r->render();
-    echo "</body></html>";
+$r->addpost(new Post($user, $l->name, "Testing 123", "#c0c0c0"));
+$r->addpost(new Post($user, $l2->name, "You know the drill.", "#00c0c0"));
+
+echo "<!doctype html><html class='no-js' lang=''><head><title>Cabaret</title>";
+echo "<link rel='stylesheet' href='style/style.css' type='text/css'/>";
+echo "</head><body>\n";
+echo $r->render();
+echo "</body></html>";
 
